22fd0a6be6d25e13e2199218ea31dfa10b338cee
Julio-C <Vector@L0002758.vectoritcgroup.com>
2019-10-21 15:48:33 -0300

    Swagger

--- src/main/java/com/api/controller/TarefaController.java
+++ src/main/java/com/api/controller/TarefaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,34 +17,44 @@ import org.springframework.web.bind.annotation.RestController;
 import com.api.model.TarefaModel;
 import com.api.repository.TarefaRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API REST TODO LIST")
+@CrossOrigin(origins = "*")
 public class TarefaController {
 	
 	@Autowired
 	TarefaRepository tarefaRepository;
 	
 	@GetMapping("/todo/list")
+	@ApiOperation(value = "Retorna a Lista de Tarefas.")
 	public List<TarefaModel> todoList(){
 		return tarefaRepository.findAll();
 	}
 	
 	@GetMapping("/todo/{id}")
+	@ApiOperation(value = "Retorna uma Tarefas por id.")
 	public TarefaModel todoId(@PathVariable(value="id") long id){
 		return tarefaRepository.findById(id);
 	}
 	
 	@DeleteMapping("/todo")
+	@ApiOperation(value = "Deleta uma Tarefas.")
 	public void deleteTodo(@RequestBody TarefaModel tarefa){
 		tarefaRepository.delete(tarefa);
 	}
 	
 	@PostMapping("/todo")
+	@ApiOperation(value = "Salva uma Tarefas.")
 	public TarefaModel save(@RequestBody TarefaModel tarefa) {
 		return tarefaRepository.save(tarefa);
 	}
 	
 	@PutMapping("/todo")
+	@ApiOperation(value = "Atualiza uma Tarefas.")
 	public TarefaModel update(@RequestBody TarefaModel tarefa) {
 		return tarefaRepository.save(tarefa);
 	}	
